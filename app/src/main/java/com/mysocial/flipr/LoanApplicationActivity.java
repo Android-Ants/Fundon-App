a04520b4c34ab31d962a8e656229892bb25526af
GantaNikhil <gantanikhil003@gmail.com>
2022-05-10 21:12:50 +0530

    Loan Application

---
+++ app/src/main/java/com/mysocial/flipr/LoanApplicationActivity.java
@@ -0,0 +1,73 @@
+package com.mysocial.flipr;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.mysocial.flipr.authentication.SignUpActivity;
+import com.mysocial.flipr.models.DetailsModel;
+import com.mysocial.flipr.models.LoanApplicationModel;
+import com.mysocial.flipr.models.User;
+import com.mysocial.flipr.viewmodels.LoanApplicationViewModel;
+import com.mysocial.flipr.viewmodels.SignInViewModel;
+import com.mysocial.flipr.viewmodels.SignUpViewModel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class LoanApplicationActivity extends AppCompatActivity {
+    LoanApplicationViewModel viewModel;
+    TextInputEditText loanamount,loantenure;
+    TextView loaninterest;
+    Button apply;
+    DetailsModel detailsModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_application);
+        loanamount=findViewById(R.id.loanamount);
+        loantenure=findViewById(R.id.loantenure);
+        loaninterest=findViewById(R.id.loaninterest);
+        apply=findViewById(R.id.apply);
+        
+        initViewModel();
+        detailsModel=new DetailsModel();
+        
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createloan();
+            }
+        });
+    }
+
+    private void createloan() {
+        LoanApplicationModel model = new LoanApplicationModel(randomid(),detailsModel.getUserName(),detailsModel.getEmail(),
+                "__","__","applied","a",Integer.valueOf(loanamount.getText().toString())
+                ,Integer.valueOf(loantenure.getText().toString()),Integer.valueOf(loaninterest.getText().toString()),true);
+        viewModel.createNewLoan(model, LoanApplicationActivity.this);
+    }
+
+    private void initViewModel() {
+        viewModel = new ViewModelProvider(this).get(LoanApplicationViewModel.class);
+        viewModel.getMessageUserObserver().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+
+            }
+        });
+
+    }
+    private String randomid(){
+        return String.valueOf(System.currentTimeMillis());
+    }
+}

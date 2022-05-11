cba7f976e86d3c15c749ca6480eae439a646de31
Ansh <anshtandonat@gmail.com>
2022-05-11 11:57:32 +0530

    request loan done

--- app/src/main/java/com/mysocial/flipr/repositories/LoanApplicationRepo.java
+++ app/src/main/java/com/mysocial/flipr/repositories/LoanApplicationRepo.java
@@ -14,6 +14,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.mysocial.flipr.cibil.User;
+import com.mysocial.flipr.models.DetailsModel;
 import com.mysocial.flipr.models.Loan;
 
 import org.json.JSONException;
@@ -25,7 +27,9 @@ import java.util.Map;
 public class LoanApplicationRepo {
 
     private static final LoanApplicationRepo instance = new LoanApplicationRepo();
-    private final MutableLiveData<String> message = new MutableLiveData<>();
+    private final MutableLiveData<String> messageLoanApply = new MutableLiveData<>();
+    private final MutableLiveData<String> messageCibil = new MutableLiveData<>();
+    private final MutableLiveData<User> cibil = new MutableLiveData<>();
     RequestQueue requestQueue;
     private final String APPLY_URL = "https://codeq-flipr.herokuapp.com/api/loan/create" ;
     private final String GET_CIBIL ="https://codeq-flipr.herokuapp.com/api/cibil/get";
@@ -64,7 +68,8 @@ public class LoanApplicationRepo {
 
                 try {
                     Toast.makeText(context, response.get("message").toString(), Toast.LENGTH_SHORT).show();
-                    message.postValue(response.get("message").toString());
+                    messageLoanApply.postValue(response.get("message").toString());
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -90,13 +95,73 @@ public class LoanApplicationRepo {
         requestQueue.add(jsonObjectRequest);
     }
 
-    public void get_cibil_details ()
+    public void get_cibil_details (Context context , DetailsModel detailsModel )
     {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userName", detailsModel.getUserName());
+        params.put("userEmail", detailsModel.getEmail());
 
+
+        JSONObject object = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, GET_CIBIL ,
+                object, new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("response", response.toString());
+
+                try {
+                    messageCibil.postValue(response.get("message").toString());
+
+                    JSONObject object1 = response.getJSONObject("cibil");
+                    User user = new User();
+                    user.setLoanCount(object1.getInt("loanCount"));
+                    user.setCurrentLoanCount(object1.getInt("currentLoanCount"));
+                    user.setFinishedOverDue(object1.getInt("finishedOverdue"));
+                    user.setCurrentLoanCount(object1.getInt("currentOverdue"));
+                    user.setSecuredLoanCount(object1.getInt("securedLoanCount"));
+                    user.setUnsecredLoanCount(object1.getInt("unsecuredLoanCount"));
+                    user.setLoanCountYear(object1.getInt("loanCountYear"));
+                    user.setDisapprovedCount(object1.getInt("disapprovedLoanCount"));
+                    user.setTotalLoanCredited(object1.getInt("totalLoanCredited"));
+                    user.setPresentLoanAmount(object1.getInt("presentLoanAmount"));
+                    user.setAmountPaid(object1.getInt("amountPaid"));
+
+                    cibil.postValue(user);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                SharedPreferences sharedPreferences = context.getSharedPreferences("All Details", Context.MODE_PRIVATE);
+                String tomken = sharedPreferences.getString("token", "");
+                Map<String, String> map = new HashMap<>();
+                map.put("Authorization", "Bearer " + tomken);
+                params.put("Content-Type", "application/json; charset=utf-8");
+                return map;
+            }
+        };
+        requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(jsonObjectRequest);
     }
 
-    public MutableLiveData<String> getMessage() {
-        return message;
+    public MutableLiveData<String> getMessageLoanApply() {
+        return messageLoanApply;
+    }
+    public MutableLiveData<String> getMessageCibil() {
+        return messageCibil;
+    }
+
+    public MutableLiveData<User> getCibil() {
+        return cibil;
     }
 
 }

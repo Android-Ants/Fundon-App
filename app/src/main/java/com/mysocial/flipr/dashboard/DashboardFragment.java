c403e0954a0227cbd2fc17779fd22a9838d184ca
Kaushan Dey <kaushandey2003@yahoo.com>
2022-10-30 22:21:59 +0530

    Dashboard progress dialog added

--- app/src/main/java/com/mysocial/flipr/dashboard/DashboardFragment.java
+++ app/src/main/java/com/mysocial/flipr/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.mysocial.flipr.dashboard;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -54,6 +55,7 @@ public class DashboardFragment extends Fragment {
     private String token ;
     private RequestQueue requestQueue ;
     private TransactionsAdapter adapter ;
+    private ProgressDialog progressDialog;
 
     private int lentAmount, borrowedAmount, avgLentRate, avgBorrowedRate, monthBorrowed, monthLent, minBorrow, minLent;
 
@@ -76,6 +78,9 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Please wait until data is loaded");
+        progressDialog.setIndeterminate(true);
         get_applied_loans();
         get_accepted_loans();
     }
